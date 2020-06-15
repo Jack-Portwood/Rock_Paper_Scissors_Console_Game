2e05ed3abbe4d344632f2b3d2b737696f36dcaa7
Jack Portwood <Jack.Portwood14@gmail.com>
2020-06-15 11:49:38 +0100

    Userinput and case statment working

--- RPS.js
+++ RPS.js
@@ -1,1 +1,37 @@
-console.log("Hello World")
+// gets users input 
+const  getUserChoice = userInput => {
+    userInput = userInput.toLowerCase(); 
+        if(userInput === 'rock'||userInput === 'paper'||userInput === 'scissors'){
+            return userInput;
+        }else{
+            console.log('Pick either Rock, Paper or Scissors')
+        }
+};
+
+let testVar = "ROCK"
+
+
+
+let getComputerChoice = () => {
+    return Math.floor(Math.random() * 3);
+};
+
+
+
+
+checkRandomNumber = (paramInput) => {
+    switch (paramInput) {
+        case 0:
+            return 'rock';
+        case 1:
+            return 'paper';
+        case 2: 
+            return 'scissor'; 
+        default:
+            return 'error: case statement failed';
+    }
+}
+
+console.log(checkRandomNumber(getComputerChoice()));
+
+console.log(getComputerChoice());
